309221f72b614d0e0a7b68b48c374ad0c822233c
Justin Benge <j.benge@techgapsolutions.com>
2018-11-05 09:43:30 -0700

    Fixed table_creation.sql

--- table_creation.sql
+++ table_creation.sql
@@ -4,7 +4,7 @@ CREATE TABLE users (username TEXT NOT NULL PRIMARY KEY, password TEXT NOT NULL, 
 CREATE TABLE authorities (username TEXT NOT NULL PRIMARY KEY REFERENCES users(username), authority TEXT NOT NULL);
 
 -- For the employee kpi table
-CREATE TABLE employee_metrics(employee_code SERIAL NOT NULL PRIMARY KEY, kpi1 DECIMAL NOT NULL, kpi2 DECIMAL NOT NULL, month INTEGER NOT NULL, year INTEGER NOT NULL, kpi3 DECIMAL NOT NULL, kpi4 DECIMAL NOT NULL, kpi_tot DECIMAL NOT NULL, text NAME);
+CREATE TABLE employee_metrics(employee_code SERIAL NOT NULL PRIMARY KEY, kpi1 DECIMAL NOT NULL, kpi2 DECIMAL NOT NULL, month INTEGER NOT NULL, year INTEGER NOT NULL, kpi3 DECIMAL NOT NULL, kpi4 DECIMAL NOT NULL, kpi_tot DECIMAL NOT NULL, text name);
 
 -- For the rule table
 CREATE TABLE rule_files (name TEXT NOT NULL PRIMARY KEY, location TEXT NOT NULL, month INTEGER NOT NULL, year INTEGER NOT NULL);
